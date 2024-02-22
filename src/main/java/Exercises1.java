fed28c8bcf72f53abdc3d9bcc75518f4ad505aab
SrgtSajjad <msajjad.za135@gmail.com>
2024-02-22 13:51:42 +0330

    fibonacci completed

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -16,9 +16,18 @@ public class Exercises1 {
     implement a function that return nth number of fibonacci series
     the series -> 1, 1, 2, 3, 5, 8, ...
     */
-    public long fibonacci(int n) {
-        // TODO
-        return 0;
+    public static long fibonacci(int n) {
+        long num1 = 1, num2 = 1, num3 = 0;
+        if (n > 2) {
+            for (int i = 0; i < n - 2; i++) {
+                num3 = num1 + num2;
+                num1 = num2;
+                num2 = num3;
+            }
+            return num3;
+        }
+
+        return 1;
     }
 
 
